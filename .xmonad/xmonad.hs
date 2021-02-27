cdbca2515d60bb0a210654e36231ca755a372fa1
Casey Avila <casey@theavilas.org>
2021-02-26 21:06:47 -0800

    Update xmobar to have faded separators

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -319,7 +319,7 @@ main = do
 workspacePP = xmobarPP { ppCurrent = xmobarColor "#ff69b4" ""
                        , ppLayout = myLayoutPrinter
                        , ppOrder = \(ws:l:t:_) -> [ws,l]
-                       , ppSep = " > "
+                       , ppSep = " <fc=#545454>|</fc> "
                        }
 
 titlePP     = xmobarPP { ppOrder = \(ws:l:t:_) -> [t]
