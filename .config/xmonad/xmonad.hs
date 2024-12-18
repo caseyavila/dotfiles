e3cff55506c3330364ae4349a79c7802014c93fc
Casey Avila <casey@theavilas.org>
2024-12-17 22:48:45 -0800

    add maim

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.ManageDocks
@@ -36,5 +37,7 @@ main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ def
     ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -1.5%"),
     ("<XF86AudioMicMute>", spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle"),
     ("<XF86MonBrightnessUp>", spawn "brightnessctl set 5%+"),
-    ("<XF86MonBrightnessDown>", spawn "brightnessctl set 5%-")
+    ("<XF86MonBrightnessDown>", spawn "brightnessctl set 5%-"),
+    ("M-s", spawn "maim --hidecursor -s ~/image-$(ls image-*.png | wc -l).png"),
+    ("M-S-s", spawn "maim --hidecursor -s | xclip -selection clipboard -t image/png")
   ]
