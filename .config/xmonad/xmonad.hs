52aeb67c38c56ca2901db7b44ee4c454a0406d76
Casey Avila <casey@theavilas.org>
2025-10-02 02:56:08 -0600

    Add ability to untile window to ceneter float

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -10,6 +10,8 @@ import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.StackSet as W
+import Data.Map as M
 
 compositor = "picom"
 
@@ -26,12 +28,16 @@ main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ def
       spacingRaw False (Border 3 3 3 3) True (Border 3 3 3 3) True $
       layoutHook def,
     startupHook = do spawnOnce compositor,
-    manageHook = checkDock --> doLower
+    manageHook = composeAll [ checkDock --> doLower ]
   }
   `additionalKeysP` [
     ("M-f", spawn "firefox"),
     ("M-p", spawn "rofi -show run"),
     ("M-c", spawn $ "killall " ++ compositor ++ " || " ++ compositor),
+    ("M-t", withFocused (\w -> windows (\s ->
+              if M.member w (W.floating s)
+                then W.sink w s
+                else W.float w (W.RationalRect (1/9) (1/7) (7/9) (5/7)) s))),
     ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
     ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +1.5%"),
     ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -1.5%"),
